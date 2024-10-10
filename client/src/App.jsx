5a584df30eb645faa64d6ab52969fafac2b81db7
Jason <jason.matillano@gmail.com>
2024-10-11 02:14:49 +0800

    added i

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Home from './Home.jsx'
 
 function App() {
-  const url = 'http://54.162.254.65:3001/'
+  const url = 'http://98.80.229.1:3001/'
   return (
     <div>
       <Home url={url}/>

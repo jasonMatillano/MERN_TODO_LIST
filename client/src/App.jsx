214a739033db66b851d3d6940e068377018453c3
Jason <jason.matillano@gmail.com>
2024-10-11 02:08:29 +0800

    added ip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Home from './Home.jsx'
 
 function App() {
-  const url = 'http://3.95.203.97:3001/'
+  const url = 'http://54.162.254.65:3001/'
   return (
     <div>
       <Home url={url}/>
